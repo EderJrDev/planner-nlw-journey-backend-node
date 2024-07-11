726a1191fb6978cadd885e6f17cfd8871ccc3699
EderJrDev <ederjuninho2003@gmail.com>
2024-07-11 13:37:21 -0300

    feat: create new routes to confirm trip and participants

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,25 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { createTrip } from "./routes/create-trip";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
+import { confirmParticipant } from "./routes/confirm-participant";
 
 const app = fastify();
 
-app.get("/teste", () => {
-  return "Hello NLW";
+app.register(cors, {
+  origin: "*",
 });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running");
