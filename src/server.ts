214994d424d1e26edb0e6f8785b4f4732e1d9430
EderJrDev <ederjuninho2003@gmail.com>
2024-07-12 09:18:02 -0300

    feat: add new db

--- src/server.ts
+++ src/server.ts
@@ -20,8 +20,12 @@ import { getParticipant } from "./routes/get-participant";
 const app = fastify();
 
 app.register(cors, {
-  credentials: true,
-  origin: "http://localhost:5173",
+  // credentials: true,
+  // origin: "http://localhost:5173",
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 });
 
 app.setValidatorCompiler(validatorCompiler);
